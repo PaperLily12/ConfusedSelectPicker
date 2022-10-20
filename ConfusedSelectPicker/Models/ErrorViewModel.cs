30ee351f5839508c8e093d54f97c9f6daed20e90
Brigid <brigidbook@gmail.com>
2022-10-20 13:57:00 -0500

    Add project files.

---
+++ ConfusedSelectPicker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConfusedSelectPicker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
